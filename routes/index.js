4c7cb7739e6316ce14bb5d07698a16d3dec78ba9
Jake Aringay <jakeraymund.aringay@gmail.com>
2021-03-19 18:13:38 +1100

    Add a GET function to retrieve all tasks from the db

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@
 const express = require("express");
 const router = express.Router();
 
+// Call database
+const db = require("../database/db.js")
+
 // Set up application
 const app = express();
 
@@ -14,6 +17,18 @@ router.get("/", (req, res) => {
     message: "Hello World",
   });
 });
+
+router.get("/tasks", (req, res) => {
+  db.any("SELECT * FROM todo")
+  .then((tasks) => {
+    res.json(tasks)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  
+});
+
 
 // Export route to app.js
 module.exports = router;
